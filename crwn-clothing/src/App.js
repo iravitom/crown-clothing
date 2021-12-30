d35985631686386e8c933424989d3d9ba0d045ea
iravitom <iravitom@googlemail.com>
2021-12-30 11:46:09 +0530

    firebase added

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,13 +1,13 @@
-import React from "react";
+import { useEffect, Component } from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shoppage/shoppage.component";
 import { SignInAndSingUp } from "./pages/sign-in-and-sing-up/sign-in-and-sing-up.component";
 import { Header } from "./components/header/header.component";
-import { auth } from "./firebase/firebase.utils";
+import { auth, db, getusers } from "./firebase/firebase.utils";
 
-class App extends React.Component {
+class App extends Component {
   constructor() {
     super();
     this.state = {
@@ -18,9 +18,8 @@ class App extends React.Component {
   unSuscribeFromAuth = null;
 
   componentDidMount() {
-    this.unSuscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
-      console.log("Signed In");
+    this.unSuscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      getusers(userAuth);
     });
   }
   componentWillUnmount() {
--- crwn-clothing/src/firebase/firebase.utils.js
+++ crwn-clothing/src/firebase/firebase.utils.js
@@ -1,5 +1,14 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import {
+  doc,
+  getDoc,
+  collection,
+  query,
+  where,
+  getDocs,
+} from "@firebase/firestore";
 
 // TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
@@ -13,4 +22,28 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+export const getusers = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  console.log(userAuth);
+  //const docRef = await getDocs(collection(db, "users", `${userAuth.uid}`));
+  //data.docs.map((doc) => console.log(doc.data()));
+  // const docRef = doc(db, "users"), where("capital", "==", true));;
+
+  const q = query(
+    collection(db, "users"),
+    where("emailId", "==", "iravitom@googlemail.com")
+  );
+  const docSnap = await getDoc(q);
+
+  if (docSnap.exists()) {
+    console.log("Document data:", docSnap.data());
+  } else {
+    // doc.data() will be undefined in this case
+    console.log("No such document!");
+  }
+};
+
+export const db = getFirestore(app);
+
 export const auth = getAuth(app);
